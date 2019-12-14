91f6ab5b2e5c0146f7330af03fe34dcde8b34293
DjCooGie <evansonchagwa01@gmail.com>
2019-12-15 01:26:39 +0300

    Added a setUp method followed by two test methods; initializing and saving methods. Defined a run condition

--- user_test.py
+++ user_test.py
@@ -9,12 +9,26 @@ class TestUser(unittest.TestCase):
         unittest.TesCast: facilitates creation of test cases
     '''
 
-def setUp(self):
-    '''
-    Method to run before test cases
-    '''
-    self.new_user = User('Evans','evan01')  
+    def setUp(self):
+        '''
+        Method to run before test cases
+        '''
+        self.new_user = User('Evans','evan01') 
 
+    def test_init(self):
+        '''
+        Checks if instantiantion of objects is properly done
+        '''
+        self.assertEqual(self.new_user.realname,'Evans')
+        self.assertEqual(self.new_user.password,'evan01')
+
+    def test_save_user(self):
+        '''
+        Checks if new user info is saved into ther users list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list),1)
+        
 
 
 
