4af40fcc3bd5af8f0247cc7a7d8eaafd6eb56eb6
DjCooGie <evansonchagwa01@gmail.com>
2019-12-14 23:21:02 +0300

    Added a constractor and passed in three arguments. Created two instance variables. Added a save_ser method

--- user.py
+++ user.py
@@ -8,4 +8,16 @@ class User:
     '''
 
     users_list = []
-  
+    def __init__(self,realname,password):
+        '''
+        Constructor defines properties each user object holds
+        '''
+
+        self.realname = realname
+        self.password = password
+
+    def save_user(self):
+        '''
+        Method saves newly created user instance
+        '''
+        User.users_list.append(self)
